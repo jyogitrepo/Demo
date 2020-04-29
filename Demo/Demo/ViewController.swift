eed632b32063e7d80871ab7205e53b34c231c978
Joe <jyohere.d@gmail.com>
2020-04-29 12:40:48 +0900

    First commit - Feature1

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("feature1")
         // Do any additional setup after loading the view.
     }
 
